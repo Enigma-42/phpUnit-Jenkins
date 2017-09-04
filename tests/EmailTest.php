5b6e00df0666dcc921053eb46de2a74020fe5db1
corentin <corentin.crl.62@gmail.com>
2017-09-04 10:43:21 +0200

    test -> Valide

--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -27,7 +27,7 @@ final class EmailTest extends TestCase
     {
         $this->assertEquals(
             'user1@example.com',
-            Email::fromString('user@example.com')
+            Email::fromString('user1@example.com')
         );
     }
 }
